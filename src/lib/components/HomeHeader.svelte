95adbffa29b5ab9075ac6a546a53ea581a8b83dc
Dao Khoi Nguyen <nguyendk.on@gmail.com>
2023-09-18 17:14:00 +0700

    first commit

---
+++ src/lib/components/HomeHeader.svelte
@@ -0,0 +1,23 @@
+<section class="py-6 px-4">
+  <div class="w-40 h-40 rounded-full overflow-hidden">
+    <img
+      src="/profile-image.png"
+      alt="Profile"
+      class="object-cover"
+    />
+  </div>
+
+  <div class="py-4 space-y-2">
+    <h2 class="text-4xl font-bold">
+      Hey! I'm Khoi Nguyen.
+    </h2>
+    <p class="text-2xl font-light">
+      I'm a full stack software engineer passionate about
+      web development, Neovim, and sharing what I know.
+    </p>
+    <p class="text-2xl font-thin">
+      You can find me on my facebook profile and I also like
+      to blog here to supplement the videos I make!
+    </p>
+  </div>
+</section>
